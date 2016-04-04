eac1ad16790df78dd5022768680ba81f62248dde
Bård Hovde <bard@cddnation.com>
2016-04-04 14:21:34 +0100

    Initial ideas push

---
+++ step1.js
@@ -0,0 +1,23 @@
+var election = require('./lib/electionEngine');
+
+var candidates = [
+    'Hilarious Clintface',
+    'Bernus Sandface',
+    'Adolph Trump'
+]
+
+var votes = [
+
+]
+
+// Get votes from API
+var votes = 
+
+// Calculate the winner
+var winner = election.getWinner();
+
+
+
+
+console.log( winner.name + ' has won the nomination with ' + winner.totalVotes + ' votes' );
+
